f5a976f34bc0031e397ccb22c740956b28d92996
g2meruem <70904882+g2meruem@users.noreply.github.com>
2023-07-29 19:43:31 -0300

    primeros dos componentes

--- template_angular/src/app/app.module.ts
+++ template_angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
